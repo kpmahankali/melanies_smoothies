06abc314cb6758096ec19c3cbefb5706ffa4d83e
kpmahankali <kpmahankali@gmail.com>
2024-05-01 10:38:35 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,6 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-# session = get_active_session()
 cnx = st.connection("snowflake")
 session = cnx.session()
 
@@ -23,23 +22,12 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
     ingredients_string = ''
-    
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-    
-    #st.write(ingredients_string)
-    
-    
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
     
-    #st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
-    
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
-    
-    #if ingredients_string:
-    #    session.sql(my_insert_stmt).collect()
-        
         st.success(('Your Smoothie is ordered,' + name_on_order + '!'), icon="✅")
